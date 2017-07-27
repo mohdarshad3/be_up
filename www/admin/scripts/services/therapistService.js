8c64acb4b9f7af38291bdd4197be5ecc0a8fdb02
cute <dsmail.arshad@gmail.com>
2017-07-27 20:54:02 +0530

    dnfff

--- www/admin/scripts/services/therapistService.js
+++ www/admin/scripts/services/therapistService.js
@@ -1,3 +1,42 @@
-/**
- * Created by Di on 7/27/2017.
- */
+angular.module('Admin').service('therapistService', function ($http, $q, $localStorage,CONFIG,Upload) {
+    return {
+        addTherapist: function (data, callback) {
+            $http({
+                method: 'POST',
+                url: CONFIG.baseUrl + 'admin/addtherapist',
+                data: data
+            }).success(function (result) {
+                callback(null, result);
+            }).error(function (err) {
+                callback(true, err);
+            });
+        },
+
+
+        getTherapist: function (page, callback) {
+            $http({
+                method: 'GET',
+                url: CONFIG.baseUrl + 'admin/getTherapistList/' + page
+            }).success(function (result) {
+                callback(null, result);
+            }).error(function (err) {
+                callback(true, err);
+            });
+        },
+
+        getTherapist: function (page, callback) {
+            $http({
+                method: 'GET',
+                url: CONFIG.baseUrl + 'admin/getUserList/' + page
+            }).success(function (result) {
+                callback(null, result);
+            }).error(function (err) {
+                callback(true, err);
+            });
+        },
+
+
+
+
+    };
+});
